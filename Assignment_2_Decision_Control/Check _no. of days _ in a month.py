d5f9d59f152bbb4fd0934dc66d5ed25f8f0268f9
silkyg <47587541+silkyg@users.noreply.github.com>
2019-02-13 18:09:09 -0800

    Delete Check _no. of days _ in a month.py

--- Assignment_2_Decision_Control/Check _no. of days _ in a month.py
+++
@@ -1,8 +0,0 @@
-# Program to take  month values in numeric format and displays the number of days in it.
-month=int(input("Enter Month number (1-12):"))
-if month in [1,3,5,7,8,10,12]:
-    print("%dth month contains 31 days in it "%month)
-elif month in [4,6,9,11]:
-    print("%dth month contains 30 days in it " % month)
-else :
-    print("%nd month contains 28 0r 29  days in it " % month)
