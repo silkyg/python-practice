8723828ec64dd18e3f4c022077f34424d346d2a1
silkyg <gognasilky@gmail.com>
2019-02-18 15:57:53 -0800

    " Assignment3"

---
+++ Assignment_4_Loop/Prime_Number.py
@@ -0,0 +1,11 @@
+#Program to check the number is prime or not
+
+n=int(input("Enter number :"))
+if n<2:
+    print("%d is not a prime number "%n)
+for i in range(2,n-1) :
+  if n%i==0:
+   print("%d is not a prime number "%n)
+   break ;
+else :
+     print("%d is a prime number "%n)
---
+++ Assignment_4_Loop/Print_n_Prime_Numbers.py
@@ -0,0 +1,21 @@
+#Program to print first N prime numbers
+def nextprime(num):
+    while True :
+        num+=1
+        for i in range(2,num):
+            if num %i==0:
+                break
+        else :
+            return num
+def Primeproducer(N):
+    num=1
+    count=1
+    while count<= N :
+        num=nextprime(num)
+        yield num
+        count+=1
+N=int(input("numbers to print "))
+l=[x for x in Primeproducer(N)]
+print(l)
+
+
